869fc660eca63baa3a91394e9687bdabc1ffdc2e
Andreas Strikos <astrikos@gmail.com>
2014-02-03 15:26:42 +0100

    create nose tests

--- test.py
+++ test.py
@@ -1,61 +1,82 @@
+"""File containing the nose tests"""
 #!/usr/bin/env python
 from datetime import datetime, timedelta
+from nose.exc import SkipTest
 from collections import namedtuple
 from ripeatlas.source import AtlasSource, AtlasChangeSource
 from ripeatlas.request import AtlasCreateRequest, AtlasChangeRequest
 from ripeatlas.measurement import Ping, Traceroute, Dns, Sslcert
 
+
 def test_create_source():
     """Unittest for sources for create request"""
-    AtlasSource(**{"type": "area", "value": "WW", "requested": 38}).build_api_struct()
+    AtlasSource(**{
+        "type": "area", "value": "WW", "requested": 38
+    }).build_api_struct()
+
 
 def test_change_source():
     """Unittest for sources for change request"""
-    remove = AtlasChangeSource(**{"value": "59", "requested": 1, "action": "remove"})
-    add = AtlasChangeSource(**{"value": "28", "requested": 1, "action": "add", "type": "probes"})
+    AtlasChangeSource(**{
+        "value": "59", "requested": 1, "action": "remove"
+    }).build_api_struct()
+    AtlasChangeSource(**{
+        "value": "28", "requested": 1, "action": "add", "type": "probes"
+    }).build_api_struct()
+
 
 def test_ping():
     """Unittest for Ping class"""
-    ping = Ping(**{
-        "target": "www.google.fr", 
+    Ping(**{
+        "target": "www.google.fr",
         "af": 4,
         "description": "testing",
         "prefer_anchors": True
     }).build_api_struct()
 
+
 def test_dns():
     """Unittest for Dns class"""
-    dns = Dns(**{
-        "target": "k.root-servers.net", "af": 4,
-        "description": "testing new wrapper", "query_type":
-        "SOA", "query_class": "IN", "query_argument": "nl",
-        "retry": 6
-    }).build_api_struct()
+    Dns(
+        **{
+            "target": "k.root-servers.net",
+            "af": 4,
+            "description": "testing",
+            "query_type": "SOA",
+            "query_class": "IN",
+            "query_argument": "nl",
+            "retry": 6
+        }
+    ).build_api_struct()
+
 
 def test_traceroute():
     """Unittest for Traceroute class"""
-    trace = Traceroute(**{
+    Traceroute(**{
         "af": 4,
         "target": 'www.ripe.net',
-        "description": 'testing'
+        "description": 'testing',
         "protocol": "ICMP",
         "prefer_anchors": True,
     }).build_api_struct()
 
+
 def test_sslcert():
     """Unittest for Sslcert class"""
-    trace = Sslcert(**{
+    Sslcert(**{
         "af": 4,
         "target": 'www.ripe.net',
-        "description": 'testing'
+        "description": 'testing',
         "prefer_anchors": True,
     }).build_api_struct()
 
+
 def test_create_request():
     """Unittest for Atlas create request"""
+    raise SkipTest("Skip create request")
     source = AtlasSource(**{"type": "area", "value": "WW", "requested": 38})
     ping = Ping(**{
-        "target": "www.google.fr", 
+        "target": "www.google.fr",
         "af": 4,
         "description": "testing",
         "prefer_anchors": True
@@ -68,7 +89,7 @@ def test_create_request():
     stop = datetime.utcnow() + timedelta(minutes=220)
     key = open("api-key", "r").readlines()[0].rstrip('\n')
     server = open("api-server", "r").readlines()[0].rstrip('\n')
-    ar = AtlasCreateRequest(
+    request = AtlasCreateRequest(
         **{
             "stop_time": stop,
             "key": key,
@@ -79,22 +100,28 @@ def test_create_request():
     )
     result = namedtuple('Result', 'success response')
     (result.success, result.response) = ar.create()
-    assert result.success == True
+    assert (result.success)
+
 
 def test_change_request():
     """Unittest for Atlas change request"""
-    remove = AtlasChangeSource(**{"value": "59", "requested": 1, "action": "remove"})
-    add = AtlasChangeSource(**{"value": "28", "requested": 1, "action": "add", "type": "probes"})
+    raise SkipTest("Skip change request")
+    remove = AtlasChangeSource(**{
+        "value": "59", "requested": 1, "action": "remove"
+    })
+    add = AtlasChangeSource(**{
+        "value": "28", "requested": 1, "action": "add", "type": "probes"
+    })
     key = open("api-key", "r").readlines()[0].rstrip('\n')
     server = open("api-server", "r").readlines()[0].rstrip('\n')
-    ar = AtlasChangeRequest(
+    request = AtlasChangeRequest(
         **{
             "key": key,
             "server": server,
             "msm_id": 1019016,
-            "sources": [remove]
+            "sources": [add, remove]
         }
     )
     result = namedtuple('Result', 'success response')
     (result.success, result.response) = ar.create()
-    assert result.success == True
+    assert (result.success)
