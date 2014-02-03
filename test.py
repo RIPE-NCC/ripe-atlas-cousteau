f1785caa05254d6bf2e5f28faa9fabd0f28cfe81
Andreas Strikos <astrikos@gmail.com>
2014-02-03 10:23:02 +0100

    build test for change participants

--- test.py
+++ test.py
@@ -1,39 +1,58 @@
 #!/usr/bin/env python
 from datetime import datetime, timedelta
 from collections import namedtuple
-from ripeatlas.source import AtlasSource
-from ripeatlas.request import AtlasRequest
+from ripeatlas.source import AtlasSource, AtlasChangeSource
+from ripeatlas.request import AtlasCreateRequest, AtlasChangeRequest
 from ripeatlas.measurement import Ping, Traceroute, Dns
 
 
 def main():
-    source = AtlasSource(**{"type": "area", "value": "WW", "requested": 38})
-    ping = Ping(**{
-        "target": "www.google.fr", 
-        "af": 4,
-        "description": "testing",
-        "prefer_anchors": True
-    })
-    dns = Dns(**{
-        "target": "k.root-servers.net", "af": 4,
-        "description": "testing new wrapper", "query_type": "SOA",
-        "query_class": "IN", "query_argument": "nl", "retry": 6
-    })
-    start = datetime.utcnow() + timedelta(minutes=120)
-    stop = start + timedelta(minutes=220)
+    #source = AtlasSource(**{"type": "area", "value": "WW", "requested": 38})
+    #ping = Ping(**{
+    #    "target": "www.google.fr", 
+    #    "af": 4,
+    #    "description": "testing",
+    #    "prefer_anchors": True
+    #})
+    #dns = Dns(**{
+    #    "target": "k.root-servers.net", "af": 4,
+    #    "description": "testing new wrapper", "query_type": "SOA",
+    #    "query_class": "IN", "query_argument": "nl", "retry": 6
+    #})
+    #start = datetime.utcnow() + timedelta(minutes=120)
+    #stop = start + timedelta(minutes=220)
+    #key = open("api-key", "r").readlines()[0].rstrip('\n')
+    #server = open("api-server", "r").readlines()[0].rstrip('\n')
+    #ar = AtlasCreateRequest(
+    #    **{
+    #        "start_time": start,
+    #        "stop_time": stop,
+    #        "key": key,
+    #        "server": server,
+    #        "measurements": [ping],
+    #        "sources": [source]
+    #    }
+    #)
+    #result = namedtuple('Result', 'success response')
+    #(result.success, result.response) = ar.create()
+    #print result.success
+    #print result.response
+    remove = AtlasChangeSource(**{"value": "69", "requested": 1, "action": "remove"})
+    add = AtlasChangeSource(**{"value": "95", "requested": 1, "action": "add", "type": "probes"})
     key = open("api-key", "r").readlines()[0].rstrip('\n')
     server = open("api-server", "r").readlines()[0].rstrip('\n')
-    ar = AtlasRequest(
+    ar = AtlasChangeRequest(
         **{
-            "start_time": start,
-            "stop_time": stop,
             "key": key,
             "server": server,
-            "measurements": [ping],
-            "sources": [source]
+            "msm_id": 1019016,
+            "sources": [add, remove]
         }
     )
     result = namedtuple('Result', 'success response')
+    print ar.url
+    ar._construct_post_data()
+    print ar.post_data
     (result.success, result.response) = ar.create()
     print result.success
     print result.response
