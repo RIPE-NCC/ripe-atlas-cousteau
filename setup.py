c157e9d175173ebb2a2dd420a40533c7efeae43c
Andreas Strikos <astrikos@ripe.net>
2014-05-27 20:12:15 +0200

    add dependency package for parsing dates + change to double quotes

--- setup.py
+++ setup.py
@@ -4,21 +4,24 @@ from setuptools import setup
 
 from ripe.atlas.cousteau.version import __version__
 
+install_requires = ["python-dateutil"],
+
 tests_require = [
-    'nose',
-    'coverage',
-    'mock',
-    'jsonschema'
+    "nose",
+    "coverage",
+    "mock",
+    "jsonschema"
 ]
 
 setup(
-    name='ripe.atlas.cousteau',
+    name="ripe.atlas.cousteau",
     version=__version__,
     packages=["ripe", "ripe.atlas", "ripe.atlas.cousteau"],
     namespace_packages=["ripe", "ripe.atlas"],
     include_package_data=True,
-    description='Python wrapper for RIPE ATLAS API',
-    author_email='atlas-dev@ripe.net',
+    description="Python wrapper for RIPE ATLAS API",
+    author_email="atlas-dev@ripe.net",
+    install_requires=install_requires,
     tests_require=tests_require,
     test_suite="nose.collector",
 )
