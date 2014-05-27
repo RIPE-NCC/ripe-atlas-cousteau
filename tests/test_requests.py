994d82a98503b0e3beaf75860cd21f39b3a6feb4
Andreas Strikos <astrikos@ripe.net>
2014-05-27 20:11:09 +0200

    add tests for new results request

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -1,12 +1,15 @@
+import re
 import unittest
+import urlparse
 from datetime import datetime, timedelta
 
 from jsonschema import validate
 
 from ripe.atlas.cousteau import (
+    Ping,
     AtlasSource, AtlasChangeSource,
     AtlasCreateRequest, AtlasChangeRequest,
-    Ping
+    AtlasResultsRequest
 )
 from . import post_data_create_schema, post_data_change_schema
 
@@ -29,6 +32,7 @@ class TestAtlasRequest(unittest.TestCase):
         request = AtlasCreateRequest(**{
             "measurements": [self.measurement], "sources": [self.create_source]
         })
+        request.build_url()
         self.assertNotEquals(getattr(request, "url", None), None)
 
 
@@ -66,3 +70,32 @@ class TestAtlasChangeRequest(unittest.TestCase):
     def test_construct_post_data(self):
         self.request._construct_post_data()
         validate(self.request.post_data, post_data_change_schema)
+
+
+class TestAtlasResultsRequest(unittest.TestCase):
+    def setUp(self):
+        self.request = AtlasResultsRequest(**{
+            "msm_id": 1000002,
+            "start": "2011-11-27",
+            "stop": "2011-11-27 01",
+            "probe_ids": [1, 2, 3]
+        })
+
+    def test_url_path(self):
+        #self.request.construct_url_path()
+        parsed_url = urlparse.urlparse(self.request.url_path)
+        self.assertEqual(
+            parsed_url.path, "/api/v1/measurement/1000002/result/"
+        )
+        print urlparse.parse_qs(parsed_url.query)
+        query_filters = urlparse.parse_qs(parsed_url.query)
+        self.assertTrue(set(
+            query_filters.keys()
+        ).issubset(["start", "stop", "prb_id"]))
+        print re.match("^/d+$", query_filters["start"][0])
+        print query_filters["start"][0]
+        self.assertTrue(re.match(r"^\d+$", query_filters["start"][0]))
+        self.assertTrue(re.match(r"^\d+$", query_filters["stop"][0]))
+        self.assertTrue(
+            re.match(r"^(\d+,)+\d+$", query_filters["prb_id"][0])
+        )
