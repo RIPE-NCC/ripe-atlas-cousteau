48e6e7559b40c5c594a004b751b78e838e076919
Andreas Strikos <astrikos@ripe.net>
2015-10-18 16:46:28 +0200

    add pypy3 to supported python implementations

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup(
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.4",
-        "Topic :: Internet :: WWW/HTTP"
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Internet :: WWW/HTTP",
     ]
 )
