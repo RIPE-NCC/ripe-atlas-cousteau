2891eb38eb7e9888c49369bfd9005dfd4f1e2b26
Francesco Iannuzzelli <fiannuzzelli@ripe.net>
2025-11-13 17:16:07 +0100

    New Sphinx theme for docs

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,7 @@ exec(open("../ripe/atlas/cousteau/version.py").read())
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -114,7 +115,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
